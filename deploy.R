328704553e542ed3c9134fc3dd73137e324dec74
Kevin Santos <kevinsantos9349@gmail.com>
2024-07-09 11:12:36 -0600

    Datos sensibles
    Se ocultaron las credenciales

--- deploy.R
+++ deploy.R
@@ -6,8 +6,8 @@ library(rsconnect)
 
 # Set your shinyapps.io account information
 rsconnect::setAccountInfo(name='tfmkfs',
-                          token='794D93A8315B500583F12E96A3D938BE',
-                          secret='97NhkCyaLtMNCpY9vyat66lg3066RUDW/Mb+obt/')
+                          token='my_token',
+                          secret='my_key')
 
 # Set the working directory to where your app.R is located
 setwd("C:/Users/KSerrano/Documents/GitHub/TFM")
